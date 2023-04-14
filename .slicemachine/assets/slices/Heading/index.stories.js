56971cd2b42a0694b6638bc26c3f6f50812447c5
Julian <vilaj46@gmail.com>
2023-04-14 13:11:30 -0400

    first commit

---
+++ .slicemachine/assets/slices/Heading/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Heading';
+
+export default {
+  title: 'slices/Heading'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Simply","spans":[]}],"description":[{"type":"paragraph","text":"Excepteur consequat anim velit. Est enim amet exercitation ad ad sunt eiusmod aute pariatur pariatur.","spans":[]}]},"id":"_Default","slice_type":"heading"}} />
+_Default.storyName = ''
